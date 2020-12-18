4c333f68431051b98563530a882aaf6ff8a1838e
Hamzah Ahmad <ahmadhamzah500@gmail.com>
2020-12-18 13:16:27 -0600

    Updates

---
+++ precache-manifest.9e874358446e7e172fba46555c1f6cf7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba100dbba05eeb3134f4b067af5b874a",
+    "url": "/react-cart-deployed/index.html"
+  },
+  {
+    "revision": "84fa3d0354a1428c55a2",
+    "url": "/react-cart-deployed/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "16c22b6399075aeef193",
+    "url": "/react-cart-deployed/static/css/main.1cc7f44b.chunk.css"
+  },
+  {
+    "revision": "84fa3d0354a1428c55a2",
+    "url": "/react-cart-deployed/static/js/2.63918d2d.chunk.js"
+  },
+  {
+    "revision": "16c22b6399075aeef193",
+    "url": "/react-cart-deployed/static/js/main.ca399bae.chunk.js"
+  },
+  {
+    "revision": "b476decbbcfd89a71423",
+    "url": "/react-cart-deployed/static/js/runtime-main.1f5d574f.js"
+  },
+  {
+    "revision": "51ea43b983be99863e3bd744efc13c78",
+    "url": "/react-cart-deployed/static/media/blue-t-shirt.51ea43b9.jpg"
+  },
+  {
+    "revision": "8fa9012b52b4df97fc820824d7b242c1",
+    "url": "/react-cart-deployed/static/media/bright-purple-t-shirt.8fa9012b.jpg"
+  },
+  {
+    "revision": "1ead1aa2f4fc08b7ab88d7500396c5bd",
+    "url": "/react-cart-deployed/static/media/cobalt-blue-t-shirt.1ead1aa2.jpg"
+  },
+  {
+    "revision": "717bb2ab3403442adceb17a749d737c7",
+    "url": "/react-cart-deployed/static/media/green-t-shirt.717bb2ab.jpg"
+  },
+  {
+    "revision": "d9a81838b28481442251b036f6097b54",
+    "url": "/react-cart-deployed/static/media/grey-t-shirt.d9a81838.jpg"
+  },
+  {
+    "revision": "96c2084a1a9c7b1a9bb16cc073ff66f8",
+    "url": "/react-cart-deployed/static/media/light-green-t-shirt.96c2084a.jpg"
+  },
+  {
+    "revision": "ee4da645b26b3741cbe4c4e7b415a2ba",
+    "url": "/react-cart-deployed/static/media/purple-t-shirt.ee4da645.jpg"
+  },
+  {
+    "revision": "b4bb683d4c9cffc23a76ed234466c563",
+    "url": "/react-cart-deployed/static/media/red-t-shirt.b4bb683d.jpg"
+  },
+  {
+    "revision": "fe47325c33f2d575942bb2009d1dcb16",
+    "url": "/react-cart-deployed/static/media/teal-t-shirt.fe47325c.jpg"
+  }
+]);
